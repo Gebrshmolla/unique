4d12e96b6d31e06f7b52b15fce0b52b528fc6f8a
Gebrshmolla <etumesi@gmail.com>
2024-05-15 09:25:46 -0700

    push in lab 2

---
+++ js.js
@@ -0,0 +1,9 @@
+// document.getElementById("form").addEventListener('submit',function (){
+function evalute(){
+    var fname=document.getElementById('fname').value;
+if(fname===""){
+    alert('file the the space')
+document.getElementById('fill').innerText="null value";
+}
+}
+
